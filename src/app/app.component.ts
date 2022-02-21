fede69f856a94927abb6e25f027ee11959e43b25
Daniel da Silva Oliveira <daniel.oliveira@azulseguros.com.br>
2022-02-20 22:32:46 -0300

    feat: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assinatura-teste';
+  assinatura: string = 'Daniel da Silva Oliveira';
+  resultado: string = '';
+  fontSelecionada: string = '';
+  resultadoFonte: string = '';
+  error: string = '';
+  imagem: any = '';
+
+  radios = [
+    {
+      value: 'font-tangerine'
+    },
+    {
+      value: 'font-dancing-script',
+    },
+    {
+      value: 'font-shadows-into-light'
+    },
+    {
+      value: 'font-caveat',
+    },
+    {
+      value: 'font-homemade-apple'
+    },
+    {
+      value: 'font-qwitcher-grypen',
+    },
+    {
+      value: 'font-herr-von-muellerhoff'
+    },
+    {
+      value: 'font-mrs-saint-delafield',
+    },
+    {
+      value: 'font-mr-de-haviland'
+    }
+  ];
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  generateImage(assinaturaSelecionada: string): void {
+    let node:any = document.getElementById(assinaturaSelecionada);
+    console.log(node);
+    htmlToImage.toPng(node).then((dataUrl) => {
+        this.imagem = this.sanitizer.bypassSecurityTrustUrl(dataUrl);
+        console.log(dataUrl);
+        // var img = new Image();
+        // img.src = dataUrl;
+        // document.body.appendChild(img);
+      }).catch(function (error) {
+        console.error('oops, something went wrong!', error);
+      });
+  }
+
+  public onSubmit(): void {
+    if (this.assinatura.trim() === '') {
+      this.error = "Adicione seu nome na assinatura"
+    }
+    this.generateImage(this.fontSelecionada);
+    this.resultado = this.assinatura;
+    this.resultadoFonte = this.fontSelecionada;
+  }
 }
